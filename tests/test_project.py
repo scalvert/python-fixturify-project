71498dc6a8e75fa34c3ac6002062150f485ccf64
Steve Calvert <scalvert@linkedin.com>
2022-10-05 13:35:28 -0700

    Reformatting

--- tests/test_project.py
+++ tests/test_project.py
@@ -36,19 +36,19 @@ def test_cleanup_dir():
 
 
 def test_get_from_files(snapshot):
-    project = Project(files={
-        'one.py': '# some python',
-        'dir': {
-            'two.py': '# another python',
-            'dir2': {
-                'three.py': '# and this makes 3!!!'
-            }
+    project = Project(
+        files={
+            "one.py": "# some python",
+            "dir": {
+                "two.py": "# another python",
+                "dir2": {"three.py": "# and this makes 3!!!"},
+            },
         }
-    })
+    )
 
-    assert project.get('one.py') == snapshot(name='one.py')
-    assert project.get('dir') == snapshot(name='dir')
-    assert project.get('dir/dir2') == snapshot(name='dir2')
+    assert project.get("one.py") == snapshot(name="one.py")
+    assert project.get("dir") == snapshot(name="dir")
+    assert project.get("dir/dir2") == snapshot(name="dir2")
 
 
 @pytest.mark.parametrize("test_input", [BAD_DIR_NAME, BAD_EMPTY_NAME])
